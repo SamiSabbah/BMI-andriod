71028e5f38f6316a7abc8fd9e02209bdd0dd8b78
Sami Sabbah <samysubbah1@gmail.com>
2021-10-30 01:15:59 +0200

    First 3 activitys

---
+++ app/src/main/java/com/example/bmi/Login.java
@@ -0,0 +1,29 @@
+package com.example.bmi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Login extends AppCompatActivity {
+
+    private TextView signupButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        signupButton = findViewById(R.id.signupButton);
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Login.this, Signup.class);
+                startActivity(i);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/bmi/Splash.java
@@ -0,0 +1,45 @@
+package com.example.bmi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+public class Splash extends AppCompatActivity {
+
+    private Handler mHandler = new Handler();
+    private TextView nextButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        nextButton = findViewById(R.id.nextButton);
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Splash.this, Login.class);
+                startActivity(i);
+                mHandler.removeCallbacksAndMessages(null);
+            }
+        });
+
+        mHandler.postDelayed(new Runnable() {
+            public void run() {
+                changeActivity();
+            }
+        }, 5000);
+
+    }
+
+    private void changeActivity() {
+        Intent i = new Intent(Splash.this, Login.class);
+
+        startActivity(i);
+    }
+}
