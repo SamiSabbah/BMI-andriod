e953df2169a2c3701bd7e49d66a0d72b51a1df62
Sami Sabbah <samysubbah1@gmail.com>
2021-12-05 00:17:32 +0200

    Add the back arrow to add food activity

--- app/src/main/java/com/example/bmi/AddFoodDetails.java
+++ app/src/main/java/com/example/bmi/AddFoodDetails.java
@@ -2,16 +2,30 @@ package com.example.bmi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class AddFoodDetails extends AppCompatActivity {
+    private TextView backArrow;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_food_details);
+
+        backArrow = findViewById(R.id.backArrow);
+
+        backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
 
         Spinner spinner = (Spinner) findViewById(R.id.category_spinner);
 
